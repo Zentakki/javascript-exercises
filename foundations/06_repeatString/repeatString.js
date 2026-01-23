d0831bd3664168f211d089bbc453c26c91d0c3e3
Zentakki <likorkaffe@gmail.com>
2026-01-23 22:29:23 +0100

    javascript-exercises/foundations/06_repeatString completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let output = "";
+    if (num < 0) {
+        output = 'ERROR';
+    }
+    for (i = 0; i < num; i++) {
+        output += string
+    }
 
-};
+    return output
+}
+console.log(repeatString('hey', 0))
 
 // Do not edit below this line
 module.exports = repeatString;
