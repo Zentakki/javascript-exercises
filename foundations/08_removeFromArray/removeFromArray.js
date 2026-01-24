9269c1b5f08125c8df882838579c7f820cb0567c
Zentakki <likorkaffe@gmail.com>
2026-01-24 08:59:59 +0100

    08_removeFromArray
    Added version without modifying the orignal
    array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,10 +1,16 @@
+// const removeFromArray = function(arr, ...rest) {
+//     for (let arg of arguments) {
+//         while (arr.includes(arg)) {
+//             arr.splice(arr.indexOf(arg), 1)
+//         }
+//     }
+//     return arr
+// };
+
+// without modifying the original array
+
 const removeFromArray = function(arr, ...rest) {
-    for (let arg of arguments) {
-        while (arr.includes(arg)) {
-            arr.splice(arr.indexOf(arg), 1)
-        }
-    }
-    return arr
+    return arr.filter((item) => !rest.includes(item))
 };
 
 // Do not edit below this line
