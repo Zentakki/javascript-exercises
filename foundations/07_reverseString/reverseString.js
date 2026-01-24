bf5b6b91044ac7306b6d484f58644ab6938bc319
Zentakki <likorkaffe@gmail.com>
2026-01-24 08:42:12 +0100

    javascript-exercises/foundations/07_reverseString completed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArr = string.split("")
+    stringArr.reverse()
+    return stringArr.join("")
 };
 
 // Do not edit below this line
