0cffca1315741639a3534fc7c8d07b950e197ea0
Zentakki <likorkaffe@gmail.com>
2026-01-24 08:56:55 +0100

    javascript-exercises/foundations/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+    for (let arg of arguments) {
+        while (arr.includes(arg)) {
+            arr.splice(arr.indexOf(arg), 1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
