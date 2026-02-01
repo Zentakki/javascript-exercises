60c2dec4887cd485452a0c9399cafa61a78625c2
Zentakki <likorkaffe@gmail.com>
2026-02-01 11:31:38 +0100

    js foundations ex 12 completed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sum = arr.reduce((total, current) => {
+        return total += current;
+    }, 0);
+    return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	const multiply = arr.reduce((total, current) => {
+        return total *= current;
+    }); // no initial value passed, it will be the first element of the array
+    return multiply
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+	return b**e;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    if (a === 0 || a === 1) return 1;
+    let total = a;
+	for (let c = a - 1; c > 0; c--) {
+        total *= c;
+    };
+    return total;
 };
 
 // Do not edit below this line
