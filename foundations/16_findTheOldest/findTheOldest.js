5db241c49bfa364eabf38ccea0875770dd26553d
Zentakki <likorkaffe@gmail.com>
2026-02-01 17:10:24 +0100

    js foundations ex 16 completed

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const copyPeople = [...people]
+    const currentAge = function(birth, death = new Date().getFullYear()) {
+        return death - birth;
+    }
 
+    copyPeople.forEach(el =>
+        el["age"] = el.yearOfDeath
+                    ? currentAge(el.yearOfBirth, el.yearOfDeath)
+                    : currentAge(el.yearOfBirth)
+    );
+    // copyPeople.sort((a, b) => {
+    //     if ( a.age < b.age) return -1;
+    //     if ( a.age > b.age) return 1;
+    //     if ( a.age = b.age) return 0;
+    // })
+    copyPeople.sort((a, b) => a.age - b.age);
+    return copyPeople.at(-1)
 };
 
 // Do not edit below this line
