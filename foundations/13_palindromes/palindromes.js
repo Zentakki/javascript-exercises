764f5e17e7eb98b8efd7311666f439fd1ab38a40
Zentakki <likorkaffe@gmail.com>
2026-02-01 11:51:57 +0100

    js foundations ex 13 completed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const validCharacters = "abcdefghijklmnopqrstuvwxyz0123456789"
+    const cleanString = string
+                            .toLowerCase()
+                            .split("")
+                            .filter(letter => validCharacters.includes(letter))
+                            .join("");
+    const cleanStringReversed = cleanString.split("").reverse().join("");
 
+    return cleanString === cleanStringReversed;
 };
+
+palindromes('A car, a man, a maraca.')
 
 // Do not edit below this line
 module.exports = palindromes;
